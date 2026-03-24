78026205496b079d6f30f37379c43ee86ddc4721
M17B30 <agrawalbhavish07@gmail.com>
2026-03-24 09:53:54 +0530

    sync

--- index.php
+++ index.php
@@ -47,20 +47,6 @@
         </div>
     </div>
     <script src="main.js"></script>
-    <script>
-        let regid = document.getElementById("register-form");
-let logid = document.getElementById("login-form")
-
-function showFormreg() {
-    regid.classList.add("active");
-    logid.classList.remove("active");
-}
-
-function showFormlog() {
-    logid.classList.add("active");
-    regid.classList.remove("active");
-}
-    </script>
 </body>
 
 </html>
